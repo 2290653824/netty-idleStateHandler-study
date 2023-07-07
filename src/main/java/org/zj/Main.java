bae0eda86579b862050a2776f9e88ea4e8375554
zhengjian <2290653824@qq.com>
2023-07-07 19:54:32 +0800

    first init

---
+++ src/main/java/org/zj/Main.java
@@ -0,0 +1,11 @@
+package org.zj;
+
+/**
+ * @author zhengjian
+ * @date 2023-07-07 19:21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
